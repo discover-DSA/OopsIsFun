d2ce044bc23995eedb5b416587d299109815abf9
akumar9 <akumar9@RSCVDI-DEV-059.tv.local>
2021-11-16 02:00:21 -0600

    Completed Upside down Hill Pattern

--- OopsIsFun/src/patternPackages/HillUpsideDown.java
+++ OopsIsFun/src/patternPackages/HillUpsideDown.java
@@ -5,6 +5,24 @@ public class HillUpsideDown {
 	public static void main(String[] args) {
 		int n=5;
 		
+		for(int i=1; i<=n; i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=i; j<n; j++)
+			{
+				System.out.print("* ");
+			}
+			for(int j=i; j<=n; j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		   
 
 	}
 
